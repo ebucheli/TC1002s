98ddfb05b9b42cda4e7e731b87f07d34cfcc910d
brilubel15 <a01022972@itesm.mx>
2020-09-15 12:21:05 -0500

    Read data lines

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,11 @@
 # io_utilities.py
-
+filepath = './data/iris.data'
 def read_vanilla(filepath):
-    pass
-    return data
+
+    with open(filepath,'r') as fp:
+        data = fp.read()
+
+data_lines = data.split('\n')
+data_final = [f.split(',')for f in data_lines]
+
+print(data_final)
