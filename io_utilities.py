0bc238011dfe7d819c72e0423b803337011a96e0
Cassabal <A01027109@itesm.mx>
2020-09-15 14:38:25 -0500

    edite io_utilities

--- io_utilities.py
+++ io_utilities.py
@@ -3,3 +3,13 @@
 def read_vanilla(filepath):
     pass
     return data
+
+    with open(filepath,'r') as fp:
+        data = fp.read()
+    data_lines = data.split('\n')
+    data_final = [f.split(',') for f in data_lines]
+    return data_final
+def main():
+    filepath = './data/iris.data'
+    data=read_vanilla(filepath)
+    print(data)
