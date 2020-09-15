1b0f89343d998fd886aca6446acc24b88c03d693
Seidmani64 <A01028650@itesm.mx>
2020-09-15 12:04:20 -0500

    Implementacion read_vanilla

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,8 @@
-# io_utilities.py
+#io_utilities.py
 
 def read_vanilla(filepath):
-    pass
+    with open(filepath, 'r') as fp:
+        data_unsorted = fp.read()
+    data_lines = data_unsorted.split('\n')
+    data = [f.split(',') for f in data_lines]
     return data
