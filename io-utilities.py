58172d15c1cce448e2d0aab0758c83d75c76df78
romuri22 <romurimex@gmail.com>
2020-09-15 12:11:03 -0500

    "Agregar read"

---
+++ io-utilities.py
@@ -0,0 +1,10 @@
+filepath = './data/iris.data'
+
+def read_vanilla(filepath):
+    with open(filepath,'r') as fp:
+        data = fp.read()
+
+    data_lines = data.split('\n')
+    data_final = [f.split(',') for f in data_lines]
+    return data_final
+print(data_final)
