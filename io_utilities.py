194abd901444d05afbbee6603c8fd0a530a0f902
Cassabal <A01027109@itesm.mx>
2020-09-15 14:50:22 -0500

    arregle codigo

--- io_utilities.py
+++ io_utilities.py
@@ -1,15 +1,20 @@
 # io_utilities.py
 
 def read_vanilla(filepath):
-    pass
-    return data
 
     with open(filepath,'r') as fp:
         data = fp.read()
     data_lines = data.split('\n')
     data_final = [f.split(',') for f in data_lines]
+
     return data_final
+
 def main():
-    filepath = './data/iris.data'
+    filepath = 'iris.data'
     data=read_vanilla(filepath)
     print(data)
+    for f in data:
+        print(f, "\n")
+
+if __name__ == '__main__':
+    main()
