01961f0f1576449bc9f511d77145867cc1b776ea
Miguel Frias <22214@oaklawnacademy.org>
2020-09-15 12:13:04 -0500

    Clase 1

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,13 @@
 # io_utilities.py
 
 def read_vanilla(filepath):
-    pass
-    return data
+    with open(filepath,'r') as fp:
+    	data = fp.read()
+
+    dataLines = data.split('\n')
+    dataFinal = []
+
+    for line in dataLines:
+    	dataFinal.append(line.split(','))
+    	
+    return dataFinal
