bf19dec393b1578fd5a264f711c7b7c5918e0aa5
Caro Ortega <caroortega@MacBook-de-Caro-2.local>
2020-09-15 12:29:21 -0500

    Agregar saludo

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,14 @@
 # io_utilities.py
 
+# io_utilities.py
+
+filepath = './data/iris.data' #r de read
+
 def read_vanilla(filepath):
-    pass
-    return data
+with open (filepath ,'r') as fp:
+    data = fp.read()
+
+data_lines = data.split('\n')
+data_final = [f.split(',')for f in data_lines]
+
+print (data_final)
