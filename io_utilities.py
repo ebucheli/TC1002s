48a373cf50f1e22f74900550465b2f52e2b591f1
Mateo González Cosio Ríos y Valles <61241231+mateoglzc@users.noreply.github.com>
2020-09-15 12:39:18 -0500

    Update io_utilities.py
    Basic Run

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,7 @@
 # io_utilities.py
-
-def read_vanilla(filepath):
-    pass
-    return data
+def read_vanilla(filename):
+    with open(filepath, 'r') as fp:
+        data = fp.read()
+    data_lines = data.split('\n')
+    data_f = [d.split(',') for d in data_lines]
+    return data_f
