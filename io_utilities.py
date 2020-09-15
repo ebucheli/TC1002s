0c1e8a5f7dd2143a820b2f9cf0c1cf0e8a7d6c82
Edgar Rostro <Edgar Rostro>
2020-09-15 12:34:07 -0500

    Cambio 1

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,12 @@
 # io_utilities.py
 
+filepath = './data/iris.data'
+
 def read_vanilla(filepath):
-    pass
+    with open(filepath, 'r') as fp:
+        data = fp.read()
+    data_lines = data.split('\n')
+    data_final = [f.split(',') for f in data_lines]
     return data
+
+print(read_vanilla(filepath))
