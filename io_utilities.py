3ce9f5b84318385375c5fffa034245e1e791169d
Ignacio-Moral-22 <a01028470@itesm.mx>
2020-09-15 12:08:21 -0500

    Change in io_utilities

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,14 @@
 # io_utilities.py
 
 def read_vanilla(filepath):
-    pass
-    return data
+    with open(filepath,'r') as fp:
+        data = fp.read()
+    data_lines = data.split('\n')
+    data_final = [f.split(',') for f in data_lines]
+
+    return data_final
+
+def main():
+    filepath = './data/iris.data'
+    data=read_vanilla(filepath)
+    print(data_final)
