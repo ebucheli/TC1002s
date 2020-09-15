5269b1fdb1ede2ac0581e9d07459460d66cd5cfa
ebucheli <edoardbucheli@gmail.com>
2020-09-15 14:25:47 -0500

    added pandas utils

--- io_utilities.py
+++ io_utilities.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def readvanilla():
     # Reads Dataset and stores results in dictionary
     # Only for python3.8
@@ -13,5 +15,34 @@ def readvanilla():
     return data
 
 
+def read_pandas(filepath, names):
+    """
+    Read a csv in pandas
+    returns a pandas dataframe
+    """
+    df = pd.read_csv(
+        filepath,
+        names=names,
+    )
+
+    return df
+
+
 if __name__ == "__main__":
-    print(readvanilla())
+    filepath = "./data/iris.data"
+    names = ['sepal_length','sepal_width','petal_length','petal_width','class']
+    df = read_pandas(filepath, names)
+    # print(df.head(10))
+    #
+    # sepal_length = df['sepal_length'].to_list()
+    # print(sepal_length)
+    #
+    # df['sl+pl'] = df['sepal_length'] + df['petal_length']
+    #
+    # print(df.head())
+
+    df_filtered = df[df['sepal_length']<4.55]
+
+    print(df_filtered)
+
+    print(df_filtered['sepal_width'].std())
